98b7877cfa41fceed88960748f7794cc3a5fcb7a
Irene Tung <irene277299@gmail.com>
2022-05-27 23:04:45 +0800

    fix: revise redux folder logic.

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import { Normalize } from 'styled-normalize';
 
-import { store } from './app/store';
+// import { store } from './store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -14,10 +14,10 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Normalize />
-      <App />
-    </Provider>
+    {/* <Provider store={store}> */}
+    <Normalize />
+    <App />
+    {/* </Provider> */}
   </React.StrictMode>
 );
 
