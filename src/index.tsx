0dd66f136052957e3b2c7ce8f22330795accbbe4
Irene Tung <irene277299@gmail.com>
2022-05-27 17:03:42 +0800

    fix: setup styled-normalize.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { Normalize } from 'styled-normalize';
 
 import { store } from './app/store';
 import App from './App';
@@ -14,6 +15,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <Normalize />
       <App />
     </Provider>
   </React.StrictMode>
