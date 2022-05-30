d565b837c20d60f4e81ec2575fe129f2bacaf78b
Irene Tung <irene277299@gmail.com>
2022-05-30 08:57:39 +0800

    fix: implement scroll to top when route change.

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { ThemeProvider } from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
 
 import theme from './styles/theme';
-
+import ScrollToTop from './components/atoms/ScrollToTop';
 import Routes from './routes';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
+        <ScrollToTop />
         <Routes />
       </BrowserRouter>
     </ThemeProvider>
