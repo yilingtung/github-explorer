ffdc3715dc9d75bf9504afc31731935a6228a466
Irene Tung <irene277299@gmail.com>
2022-05-30 09:19:52 +0800

    fix: revise LanguageTag styles.

--- src/components/molecules/LanguageTag/styles.tsx
+++ src/components/molecules/LanguageTag/styles.tsx
@@ -7,11 +7,16 @@ interface ColorBoxProps {
 export const Container = styled.div`
   display: inline-flex;
   align-items: center;
+  position: relative;
 `;
 
 export const ColorBox = styled.div<ColorBoxProps>`
   display: inline-flex;
   flex-shrink: 0;
+  position: absolute;
+  left: 0;
+  top: 50%;
+  transform: translateY(-50%);
   height: 12px;
   width: 12px;
   margin-right: 8px;
