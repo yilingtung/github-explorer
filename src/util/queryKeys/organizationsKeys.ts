7628f9048a2d45b74a3c9a9299b9d7f320bf96ec
Irene Tung <irene277299@gmail.com>
2022-06-05 09:35:40 +0800

    fix: remove useless query key.

--- src/util/queryKeys/organizationsKeys.ts
+++ src/util/queryKeys/organizationsKeys.ts
@@ -1,6 +1,5 @@
 export const organizationsKeys = {
   all: ['organizations'] as const,
-  lists: () => [...organizationsKeys.all, 'list'] as const, // ['organizations', 'list']
   details: () => [...organizationsKeys.all, 'detail'] as const, // ['organizations', 'detail']
   detail: (name: string) => [...organizationsKeys.details(), name] as const, // ['organizations', 'detail', {name}]
 };
