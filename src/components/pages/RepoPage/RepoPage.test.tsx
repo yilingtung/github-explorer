a59829c641a272bc2ebf444359967c5d0d33f5a9
Irene Tung <irene277299@gmail.com>
2022-06-10 11:51:15 +0800

    test: add RepoPage testing.

---
+++ src/components/pages/RepoPage/RepoPage.test.tsx
@@ -0,0 +1,34 @@
+import { fireEvent, screen } from '@testing-library/react';
+import { Router, Routes, Route } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
+import { customRender } from '../../../util/tests/test-utils';
+import RepoPage from '.';
+
+describe('RepoPage page', () => {
+  test('should show repo data', async () => {
+    const history = createMemoryHistory();
+    history.push('/facebook/react');
+
+    const result = customRender(
+      <Router location={history.location} navigator={history}>
+        <Routes>
+          <Route path="/:org/:repo" element={<RepoPage />} />
+        </Routes>
+      </Router>,
+      {
+        theme: true,
+        queryClient: true,
+      }
+    );
+
+    // check the routing first
+    expect(history.location.pathname).toBe('/facebook/react');
+
+    // should show the organization name
+    expect(await result.findByText(/facebook/i)).toBeInTheDocument();
+
+    // should show readme data
+    expect(await result.findByText(/mocked getReadme/i)).toBeInTheDocument();
+  });
+});
